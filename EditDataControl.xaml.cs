d6b2ecba930a6ce622cb0b91167bd5bdae913d95
StasTikhomirov <Stastih97@yandex.ru>
2020-10-02 16:41:16 +0300

    UC для изменения данных

---
+++ EditDataControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunnyVodovoz
+{
+    /// <summary>
+    /// Логика взаимодействия для EditDataControl.xaml
+    /// </summary>
+    public partial class EditDataControl : UserControl
+    {
+        public EditDataControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
